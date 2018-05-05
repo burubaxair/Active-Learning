87d9712276cb0abed71f02e07a55a9600e037894
Vladislav Gladkikh <vladgladkikh@gmail.com>
2018-05-05 23:26:27 +0900

    Add files via upload

---
+++ actclf01.py
@@ -0,0 +1,75 @@
+# A script that uses Logistic Regression 
+# for the exersice at page 13, Figure 2.2. 
+# of the book "Active Learning" by Burr Settles, 
+# Link to the book: http://active-learning.net/
+
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+np.random.seed(1)
+
+mu1 = np.array([-2,0])
+mu2 = np.array([2,0])
+
+sig = 1
+
+n_points = 200
+
+cl1 = sig * np.random.randn(n_points,2) + mu1
+cl2 = sig * np.random.randn(n_points,2) + mu2
+
+y1 = np.zeros((n_points,1))
+y2 = np.ones((n_points,1))
+
+X = np.vstack((cl1,cl2))
+y = np.vstack((y1,y2))
+
+ind1 =  np.random.choice(range(n_points), size=1, replace=False)
+ind2 =  np.random.choice(range(n_points), size=1, replace=False)
+
+X1_train, X1 = cl1[ind1,:], cl1[[i for i in range(n_points) if i not in ind1],:]
+y1_train, y1 = y1[ind1,:], y1[[i for i in range(n_points) if i not in ind1],:]
+X2_train, X2 = cl2[ind2,:], cl2[[i for i in range(n_points) if i not in ind2],:]
+y2_train, y2 = y2[ind2,:], y2[[i for i in range(n_points) if i not in ind2],:]
+
+X = np.vstack((X1,X2))
+y = np.vstack((y1,y2))
+
+X_train = np.vstack((X1_train,X2_train))
+y_train = np.vstack((y1_train,y2_train))
+
+logreg = LogisticRegression()
+
+n_iter = 15 # number of iterations
+
+for i in range(n_iter):
+
+    logreg.fit(X_train, y_train.ravel())
+    pred = logreg.predict_proba(X)
+    ind_min_conf = np.argmin(np.absolute(pred-0.5),axis=0)[0]
+    print('Least confident: ',X[ind_min_conf,:],y[ind_min_conf],'\n')
+
+    w = logreg.coef_[0]
+    a = -w[0] / w[1]
+
+    xx = np.linspace(-4,4)
+    yy = a * xx - (logreg.intercept_[0]) / w[1]
+
+    class_colors = ["green", "red"]
+    plt.scatter(cl1[:,0], cl1[:,1], s = 2, marker='.', color='green')
+    plt.scatter(cl2[:,0], cl2[:,1], s = 2, marker='.', color='red')
+    plt.scatter(X_train[:,0], X_train[:,1], s = 50, marker='s', c=class_colors)
+    plt.plot(xx, yy, 'b-')
+    plt.xlim((-5, 5))   
+    plt.ylim((-5, 5))
+    plt.title('Iteration '+str(i+1))
+    plt.grid()
+    plt.show()
+
+    X_train = np.vstack((X_train,X[ind_min_conf,:]))
+    y_train = np.vstack((y_train,y[ind_min_conf,:]))
+
+    X = X[[i for i in range(len(X)) if i != ind_min_conf],:]
+    y = y[[i for i in range(len(y)) if i != ind_min_conf],:]
+
---
+++ actreg01.py
@@ -0,0 +1,63 @@
+# A script that uses Gaussian Process Regressor 
+# for the exersice at page 18, Figure 2.6. 
+# of the book "Active Learning" by Burr Settles, 
+# Link to the book: http://active-learning.net/
+
+# See also https://stackoverflow.com/questions/50171114/gaussian-process-regression-sudden-increase-of-the-predictions-variance
+
+import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+np.random.seed(1)
+
+
+x = np.atleast_2d(np.linspace(-10,10,200)).T
+
+mu = 0
+sig = 2
+
+def gaussian(x, mu, sig):
+    return np.exp(-np.square((x-mu)/sig)/2)
+
+x_train = np.atleast_2d(sig * np.random.randn(1,2) + mu).T
+#training data
+
+def fit_GP(x_train):
+
+    y_train = gaussian(x_train, mu, sig).ravel()
+
+    # Instanciate a Gaussian Process model
+    kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
+    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=9)
+
+    # Fit to data using Maximum Likelihood Estimation of the parameters
+    gp.fit(x_train, y_train)
+
+    # Make the prediction on the meshed x-axis (ask for MSE as well)
+    y_pred, sigma = gp.predict(x, return_std=True)
+    return y_train, y_pred, sigma
+
+def plot_GP(n_iter):
+
+    f, ax = plt.subplots(2, sharex=True)
+    ax[0].set_title('Iteration '+str(n_iter+1))
+    ax[0].plot(x, gaussian(x, mu, sig), color="red", label="ground truth")
+    ax[0].scatter(x_train, y_train, color='navy', s=30, marker='o', label="training data")
+    ax[0].plot(x, y_pred, 'b-', color="blue", label="prediction")
+    ax[0].fill(np.concatenate([x, x[::-1]]),
+             np.concatenate([y_pred - 1.9600 * sigma,
+                            (y_pred + 1.9600 * sigma)[::-1]]),
+             alpha=.3, fc='b', ec='None', label='95% conf. intvl')
+    ax[0].legend(loc='best')
+    ax[1].plot(x,sigma)
+    plt.savefig('gp'+str(n_iter+1)+'.png')
+    plt.show()
+
+
+n_iter = 15 # number of iterations
+
+for i in range(n_iter):
+    y_train, y_pred, sigma = fit_GP(x_train)
+    plot_GP(i)
+    x_train = np.vstack((x_train, x[np.argmax(sigma)]))
